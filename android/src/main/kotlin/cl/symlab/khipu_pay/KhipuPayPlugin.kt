acf64d45246a92b514ed19f90dc4e1d24704d0d8
ErnestoGN <egonzalez@paymentez.com>
2022-03-15 16:16:35 -0600

    upgrade to Android12

--- android/src/main/kotlin/cl/symlab/khipu_pay/KhipuPayPlugin.kt
+++ android/src/main/kotlin/cl/symlab/khipu_pay/KhipuPayPlugin.kt
@@ -10,61 +10,63 @@ import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** KhipuPayPlugin */
-class KhipuPayPlugin: FlutterPlugin, ActivityAware {
-  private var flutterPluginBinding: FlutterPlugin.FlutterPluginBinding? = null
-  private var methodCallHandler: MethodCallHandlerImpl? = null
+class KhipuPayPlugin : FlutterPlugin, ActivityAware {
+    private var flutterPluginBinding: FlutterPlugin.FlutterPluginBinding? = null
+    private var methodCallHandler: MethodCallHandlerImpl? = null
 
-  private var binding: ActivityPluginBinding? = null
+    private var binding: ActivityPluginBinding? = null
 
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects.
-  companion object {
-    private var registrar: Registrar? = null
+    // This static function is optional and equivalent to onAttachedToEngine. It supports the old
+    // pre-Flutter-1.12 Android projects.
+    companion object {
+        private var registrar: Registrar? = null
 
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      this.registrar = registrar
-      KhipuPayPlugin().maybeStartListening(registrar.activity(), registrar.messenger())
+        @JvmStatic
+        fun registerWith(registrar: Registrar) {
+            this.registrar = registrar
+            registrar.activity()
+                ?.let { KhipuPayPlugin().maybeStartListening(it, registrar.messenger()) }
+        }
     }
-  }
 
-  override fun onAttachedToEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    this.flutterPluginBinding = binding
-  }
-
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    this.flutterPluginBinding = null
-  }
-
-  // ActivityAware
-  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-    onAttachedToActivity(binding)
-  }
-
-  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-    this.binding = binding
-    maybeStartListening(binding.activity, flutterPluginBinding!!.binaryMessenger)
-  }
-
-  override fun onDetachedFromActivityForConfigChanges() {
-    onDetachedFromActivity()
-  }
-
-  override fun onDetachedFromActivity() {
-    if(methodCallHandler == null) return
-
-    methodCallHandler!!.stopListening()
-    methodCallHandler = null
-  }
-
-  // Initialize MethodCallHandler
-  private fun maybeStartListening(activity: Activity, messenger: BinaryMessenger){
-    methodCallHandler = MethodCallHandlerImpl(activity, messenger)
-
-    if(registrar != null){
-      registrar!!.addActivityResultListener(methodCallHandler)
-    }else{
-      binding!!.addActivityResultListener(methodCallHandler!!)
+    override fun onAttachedToEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        this.flutterPluginBinding = binding
     }
-  }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        this.flutterPluginBinding = null
+    }
+
+    // ActivityAware
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        onAttachedToActivity(binding)
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        this.binding = binding
+        maybeStartListening(binding.activity, flutterPluginBinding!!.binaryMessenger)
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        onDetachedFromActivity()
+    }
+
+    override fun onDetachedFromActivity() {
+        if (methodCallHandler == null) return
+
+        methodCallHandler!!.stopListening()
+        methodCallHandler = null
+    }
+
+    // Initialize MethodCallHandler
+    private fun maybeStartListening(activity: Activity, messenger: BinaryMessenger) {
+        methodCallHandler = MethodCallHandlerImpl(activity, messenger)
+        methodCallHandler?.let {
+            if (registrar != null) {
+                registrar!!.addActivityResultListener(it)
+            } else {
+                binding!!.addActivityResultListener(it)
+            }
+        }
+    }
 }
